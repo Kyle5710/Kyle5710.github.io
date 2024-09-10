58725f0614c03be6d4f636ec6a70ac19b65c74bc
Kyle5710 <7006960@spsd.sk.ca>
2024-09-10 15:18:54 -0600

    First Project

---
+++ 00 FIRST P5 PROJECT/sketch.js
@@ -0,0 +1,16 @@
+// Project Title
+// Your Name
+// Date
+// One line description
+
+
+function setup() {
+  // happens exactly once, at the start of our program
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  // happens over and over again (60 fps)
+  // background(220);
+  circle(mouseX, mouseY, 30);
+}
