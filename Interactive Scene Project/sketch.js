83249d5f05da71c4686f9e161c9ade09f0832e8e
Kyle5710 <7006960@spsd.sk.ca>
2024-09-17 15:20:27 -0600

    Made a house, clouds (need to fix), colors

--- Interactive Scene Project/sketch.js
+++ Interactive Scene Project/sketch.js
@@ -1,37 +1,62 @@
-// Interactive Scene Project
-// Kyle MacLean
-// September 16, 2024
-// An scene that the user can interact with
+//Interactive Scene Project
+//Kyle MacLean
+//September 16, 2024
+//An scene that the user can interact with
 
-// background vars
+//background vars
 let currentBack = 0;
-let currentBackColor = "green";
+let currentBackColor = "cyan";
+
+//cloud x and y positions
+let xPos = 150;
+let yPos = 350;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
+}
+
+function draw(){
   background(currentBackColor);
   noStroke();
 
-  // draw our clouds
-  for(let i = 0; i < 4; i++){
-    circle(200, 100, 75);
-    circle(100, 100, 75);
-    circle(125, 125, 75);
-    circle(105, 73, 75);
-    circle(170, 60, 75);
-    circle(145, 70, 75);
-    circle(150, 120, 75);
-    print("hello");
-  }
+  //draw our clouds
+  fill("white");
+  cloud();
+
+  //draw ground
+  fill("Tan");
+  rect(0, 3000, 8000, 1000);
+
+  //artist mark
+  fill("black");
+  textSize(200);
+  text("Kyle", 7000, 3750);
+
+  //house
+  fill("yellow");
+  rect(2000, 2200, 1000, 800);
+  fill("red");
+  triangle(1950, 2225, 2500, 1600, 3050, 2225);
+  fill("brown");
+  rect(2500, 2500, 300, 500);
+  fill("gold");
+  circle(2725, 2750, 75);
+  fill("lightblue");
+  rect(2050, 2500, 400, 250);
 }
 
-function draw() {
-  background("blue");
-  
-  
+//fix backgrounds, add character, both mouse and keyboard input, use currentBack var
+
+function cloud(){
+  let randomInt = random(375, 525);
+  xPos += randomInt;
+  circle(xPos + 160, yPos, 400);
+  circle(xPos - 100, yPos, 400);
+  circle(xPos - 125, yPos + 70, 400);
+  circle(xPos - 145, yPos - 90, 400);
+  circle(xPos + 140, yPos - 76, 400);
+  circle(xPos - 35, yPos - 140, 400);
+  circle(xPos, yPos + 120, 400);
+  xPos = 0;
 }
-
-// fix backgrounds, make the clouds not spawn ontop of each other, add character, artist mark, both mouse and keyboard input, use currentBack var
-
-
 
