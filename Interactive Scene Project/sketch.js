5efec5469781f99914b798d4686f9e8220b12f41
Kyle5710 <7006960@spsd.sk.ca>
2024-09-18 15:20:25 -0600

    added character, fixed cloud, need to fix changing the background with middle mouse button

--- Interactive Scene Project/sketch.js
+++ Interactive Scene Project/sketch.js
@@ -3,20 +3,22 @@
 //September 16, 2024
 //An scene that the user can interact with
 
-//background vars
+//change background vars
 let currentBack = 0;
 let currentBackColor = "cyan";
 
 //cloud x and y positions
 let xPos = 150;
-let yPos = 350;
+let yPos = 550;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw(){
-  background(currentBackColor);
+  //change the background when we hit the middle mouse button
+  changeBackground();
+
   noStroke();
 
   //draw our clouds
@@ -26,11 +28,6 @@ function draw(){
   //draw ground
   fill("Tan");
   rect(0, 3000, 8000, 1000);
-
-  //artist mark
-  fill("black");
-  textSize(200);
-  text("Kyle", 7000, 3750);
 
   //house
   fill("yellow");
@@ -43,12 +40,19 @@ function draw(){
   circle(2725, 2750, 75);
   fill("lightblue");
   rect(2050, 2500, 400, 250);
+
+  //draw character and make it follow the mouse
+  character();
+
+  //artist mark
+  fill("black");
+  textSize(200);
+  text("Kyle", 7000, 3750);
 }
 
-//fix backgrounds, add character, both mouse and keyboard input, use currentBack var
 
 function cloud(){
-  let randomInt = random(375, 525);
+  let randomInt = random(14,15);
   xPos += randomInt;
   circle(xPos + 160, yPos, 400);
   circle(xPos - 100, yPos, 400);
@@ -57,6 +61,49 @@ function cloud(){
   circle(xPos + 140, yPos - 76, 400);
   circle(xPos - 35, yPos - 140, 400);
   circle(xPos, yPos + 120, 400);
-  xPos = 0;
+  if(xPos > 8000){
+    xPos = 0;
+  }
 }
 
+
+function character(){
+  fill(254, 219, 177);
+  rect(mouseX - 250, mouseY - 225, 500, 500, 500);
+  fill("green");
+  circle(mouseX - 110, mouseY - 20, 60);
+  circle(mouseX + 110, mouseY - 20, 60);
+  fill(247, 111, 111);
+  rect(mouseX - 100, mouseY + 100, 200, 40, 0, 0, 70);
+}
+
+
+function changeBackground(){
+  background(currentBackColor);
+
+  if(mouseButton === "LEFT"){
+    if(currentBack < 3){
+      currentBack = 0;
+    }
+
+    else currentBack += 1;
+  }
+
+  if(currentBack === 0){
+    currentBackColor = "cyan";
+  }
+
+  if(currentBack === 1){
+    currentBackColor = "green";
+  }
+
+  if(currentBack === 2){
+    currentBackColor = "purple";
+  }
+
+  if(currentBack === 3){
+    currentBackColor = "black";
+  }
+
+  print(currentBack);
+}
