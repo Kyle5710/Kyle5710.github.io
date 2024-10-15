02dcd05b0faa221103d01b79fdf36c9d90d511e7
Kyle5710 <7006960@spsd.sk.ca>
2024-10-15 14:29:27 -0600

    .

---
+++ Create your first Class/sketch.js
@@ -0,0 +1,15 @@
+// Create your First Class
+// Kyle MacLean
+// October 15, 2024
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
+
+class roundRacer(xPos, color){
+  
+}
