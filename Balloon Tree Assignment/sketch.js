d6c7097d0d7da2e3bd0653d081f754f0ce42b114
Kyle5710 <7006960@spsd.sk.ca>
2024-11-14 15:19:46 -0600

    drew leaves, trunk weight, random colors, set randomSeed

--- Balloon Tree Assignment/sketch.js
+++ Balloon Tree Assignment/sketch.js
@@ -4,32 +4,54 @@
 //
 
 let scale = 15;
+let seed;
 
 function setup() {
   createCanvas(500, 500);
   background(255);
-  
+  seed = random(100);
+
 }
 
 function draw() {
-  drawTree(width/2, height*0.9, 90, 6);
+  drawTree(width / 2, height * 0.9, 90, 6);
+  randomSeed(seed);
 }
 
-function drawLine( x1,  y1,  x2,  y2,  depth) {
+function drawLine(x1, y1, x2, y2, depth) {
   //draw a line segment connecting (x1,y1) to (x2,y2)
+  //branch weight changed depending on depth
+  strokeWeight(depth / 2);
   line(x1, y1, x2, y2);
+
+}
+
+function drawLeaf(x2, y2, depth) {
+  //leaf properties
+  fill(color(random(255), random(255), random(255)));
+  strokeWeight(1.5);
+
+  //draw leaf
+  circle(x2, y2, random());
 }
 
 function drawTree(x1, y1, angle, depth) {
   if (depth > 0) {
-    let x2 = x1 + cos(radians(angle))*depth*scale;     //calculate endpoints of current branch
-    let y2 = y1 - sin(radians(angle))*depth*scale;     //using trig ratios. Get shorter based on depth
+    let x2 = x1 + cos(radians(angle)) * depth * scale;     //calculate endpoints of current branch
+    let y2 = y1 - sin(radians(angle)) * depth * scale;     //using trig ratios. Get shorter based on depth
 
-    drawLine(x1, y1, x2, y2, depth);
+    drawLine(x1, y1, x2, y2, depth * 1.5);
+
+    //draw leaves on branches with depth < 5
+    if (depth < 5) {
+      drawLeaf(x2, y2, depth);
+    }
 
     //3-branch tree
-    drawTree(x2, y2, angle, depth-1);
-    drawTree(x2, y2, angle-18, depth-1);
-    drawTree(x2, y2, angle+18, depth-1);
+    drawTree(x2, y2, angle, depth - 1);
+    drawTree(x2, y2, angle - 18, depth - 1);
+    drawTree(x2, y2, angle + 18, depth - 1);
   }
 }
+
+//create new var called userDepth to tell when to draw the leaves also fix drawLeaf
